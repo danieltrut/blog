40e034cda4ae2c0a1766762b0dc9c09b0f5466ea
danieltrut <Avdayg7410>
2021-02-22 17:32:43 +0200

    Lisatud raamistiku failid

---
+++ app/libs/Core.php
@@ -0,0 +1,53 @@
+<?php
+
+
+class Core
+{ // class begin
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $params = [];
+    // constructor
+
+    // get url data
+    /**
+     * Core constructor.
+     */
+    public function __construct()
+    {
+        $url = $this->getUrl();
+        $controllerName = ucwords($url[0]);
+        $controllerFile = '../app/controllers/'.$controllerName.'.php';
+        if(file_exists($controllerFile)) {
+            $this->currentController = $controllerName;
+            unset($url[0]);
+        }
+        require_once '../app/controllers/'.$this->currentController.'.php';
+        $this->currentController = new $this->currentController;
+
+        if(method_exists($this->currentController, $url[1])) {
+            $this->currentMethod = $url[1];
+            unset($url[1]);
+        }
+        $this->params = $url ? array_values($url) : array();
+
+        call_user_func_array(array($this->currentController, $this->currentMethod), $this->params);
+
+
+        /**
+        print_r($this->params); echo $controllerName.'<br>';
+        print_r($this ->currentController);
+        echo $this->currentMethod.'<br>';
+        print_r($url); */
+    }
+
+    public function getUrl(){
+        if(isset($_GET['url'])){
+            $url = $_GET['url'];
+            $url = rtrim($url, '/');
+            $url = htmlentities($url);
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+    }
+} // class end
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once '../app/bootstrap.php';
+// conf test
+
+// init app core
+$init = new Core();
