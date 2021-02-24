2875420394c36c85281253233161dbbcbc0f1e25
danieltrut <Avdayg7410>
2021-02-24 10:37:12 +0200

    Loodud controller, mudel ja vaade failid kasutajate halduseks.

---
+++ app/controllers/Users.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Users extends Controller
+{
+    /**
+     * Users constructor.
+     */
+    public function __construct()
+    {
+        $usersModel = $this->model('User');
+    }
+    public function register(){
+        $this->view('users/register');
+    }
+    public function login(){
+        $this->view('users/login');
+    }
+}
